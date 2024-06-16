a0f046803dc69ccda945b5d8a2c430615f48eea1
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-06-16 15:46:38 -0300

    feat: feito integração com realm para mostar dados de usuario em componente header e logout implementado

---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { TouchableOpacity } from 'react-native'
+import { Power } from 'phosphor-react-native'
+import { useUser, useApp } from '@realm/react'
+
+import theme from '@/theme'
+
+import { Container, Greeting, Message, Name, Picture } from './styles'
+
+export function HomeHeader() {
+  const user = useUser()
+  const app = useApp()
+
+  function handleLogout() {
+    app.currentUser?.logOut()
+  }
+
+  return (
+    <Container>
+      <Picture
+        source={{ uri: user.profile.pictureUrl }}
+        placeholder="U184i9offQof00ayfQay~qj[fQj[9FfQfQfQ"
+      />
+
+      <Greeting>
+        <Message>Olá</Message>
+
+        <Name>{user.profile.name}</Name>
+      </Greeting>
+
+      <TouchableOpacity activeOpacity={0.7} onPress={handleLogout}>
+        <Power size={32} color={theme.COLORS.GRAY_400} />
+      </TouchableOpacity>
+    </Container>
+  )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+
+import { HomeHeader } from '@/components/HomeHeader'
+
 import { Container } from './styles'
 
 export function Home() {
-  return <Container></Container>
+  return (
+    <Container>
+      <HomeHeader />
+    </Container>
+  )
 }
