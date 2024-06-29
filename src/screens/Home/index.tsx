9653a4f223b87242462ae5a00ab068a2c50f5e90
brunodsazevedo <bruno.ds.azevedo@gmail.com>
2024-06-29 19:33:14 -0300

    feat: fetio fluxo ara registro de chegadas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import { Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
@@ -39,10 +40,13 @@ export function Home() {
   }
 
   useEffect(() => {
+    fetchVehicleInUse()
+  }, [])
+
+  useEffect(() => {
     realm.addListener('change', () => fetchVehicleInUse())
 
     return () => realm.removeListener('change', fetchVehicleInUse)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
